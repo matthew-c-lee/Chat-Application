099406ba3db23e91afdb6deb0a7acedac9e48a08
Mikalai Asetski <masetski@email.uscupstate.edu>
2021-09-09 11:45:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('Mikalai is testing...')
+    print('Mikalai Asetski is testing...')
     print('Jonathan is testing...')
     print('Robert is testing...')
 
