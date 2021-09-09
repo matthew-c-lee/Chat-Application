b7cc75f3b9f1e69976134430a6c7c66e522fdb54
mcl7-upstate <77367175+mcl7-upstate@users.noreply.github.com>
2021-09-09 12:43:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Mikalai Asetski is testing...')
+    print('Jonathan is testing...')
+    print('Robert is testing...')
 
 
 
