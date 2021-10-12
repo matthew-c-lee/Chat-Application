b5d75c801f468b131e9915b2c0d00aab0d0a33bb
mcl7-upstate <77367175+mcl7-upstate@users.noreply.github.com>
2021-10-11 23:41:42 -0400

    notes

--- views.py
+++ views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for, jsonify
+import json
+from . import db
 
 views = Blueprint(__name__, "views")
 
@@ -28,3 +30,15 @@ def get_data():
 @views.route("/go-to-home")
 def go_to_home():
     return redirect(url_for("views.home"))
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['note']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    
+    return jsonify({})
