772c963f2b0e2667a7ead0d12ddc12c9bd81233a
mcl7-upstate <77367175+mcl7-upstate@users.noreply.github.com>
2021-10-11 22:17:26 -0400

    update

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_login.utils import login_required
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -13,6 +15,8 @@ def create_app():
     db.init_app(app)
 
 
+
+
     from .views import views
     from .auth import auth
 
@@ -22,6 +26,14 @@ def create_app():
     from .models import User, Note
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
