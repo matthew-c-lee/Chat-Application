7d8d8f05788528d5fc80c32b6682f91dbad693b3
mcl7-upstate <77367175+mcl7-upstate@users.noreply.github.com>
2021-10-13 19:57:58 -0400

    changed email to username

--- website/auth.py
+++ website/auth.py
@@ -10,10 +10,10 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get('email')
+        username = request.form.get('username')
         password = request.form.get('password')
 
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully.', category='success')
@@ -38,17 +38,17 @@ def logout():
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
-        email = request.form.get('email')
+        username = request.form.get('username')
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
      
         if user:
-            flash('Email already exists.', category='error')
-        elif len(email) < 4:  # password too short
-            flash('Email must be at least 4 characters.',
+            flash('Username taken.', category='error')
+        elif len(username) < 4:  # password too short
+            flash('Username must be at least 4 characters.',
                   category='error')  # tell the user
         elif len(first_name) < 2:
             flash('First name must be at least 2 characters.', category='error')
@@ -57,7 +57,7 @@ def sign_up():
         elif password1 != password2:
             flash('Passwords do not match.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(username=username, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
 
