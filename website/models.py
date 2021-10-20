6a4457e41cf4d9afa5e7b67636ec509521d96c54
mcl7-upstate <77367175+mcl7-upstate@users.noreply.github.com>
2021-10-20 14:58:57 -0400

    -

--- website/models.py
+++ website/models.py
@@ -10,6 +10,7 @@ from sqlalchemy.sql import func
 #     to_user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #one-to-many relationship
 
 
+
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
