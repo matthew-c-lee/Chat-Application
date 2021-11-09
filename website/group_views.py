fad98938f0f33f105788d9907b86c5a7a2225fed
mcl7-upstate <77367175+mcl7-upstate@users.noreply.github.com>
2021-11-09 00:59:11 -0500

    bug fix

--- website/group_views.py
+++ website/group_views.py
@@ -23,15 +23,34 @@ def create_group():
             db.session.commit()
 
         flash("Created " + group_name)
-        return redirect(url_for('views.chat'))
+        group = Group.query.filter(Group.group_name == group_name)[-1]
+
+        return redirect('/group-chat/' + str(group.group_id))
 
     return render_template("create_group.html", user=current_user)
 
-@group_views.route('/group-chat/<string:group_chat>', methods=['GET', 'POST'])
+@group_views.route('/change-group-name/<string:group_id>', methods=['GET', 'POST'])
+def change_group_name(group_id):
+    group = Group.query.filter(Group.group_id == group_id).first_or_404()
+
+    if request.method == 'POST':
+        new_name = request.form.get('group-name')
+        
+        if len(new_name) > 0:
+            group.group_name = new_name
+            db.session.commit()
+
+        # flash("Created " + group_name)
+        return redirect('/group-chat/' + str(group.group_id))
+
+    return render_template("change_group_name.html", user=current_user)
+
+# the actual group chat
+@group_views.route('/group-chat/<string:group_id>', methods=['GET', 'POST'])
 @login_required
-def group_chat(group_chat):
+def group_chat(group_id):
     # find the actual group from the name
-    group = Group.query.filter(Group.group_name == group_chat).first_or_404()
+    group = Group.query.filter(Group.group_id == group_id).first_or_404()
 
     if request.method == 'POST': #if button is pressed
         message = request.form.get('message')
@@ -43,7 +62,7 @@ def group_chat(group_chat):
             db.session.add(new_message)
             db.session.commit()
             # flash('Message sent.', category='success')
-            return redirect('/group-chat/' + group.group_name)
+        return redirect('/group-chat/' + str(group.group_id))
 
     # east = timezone('US/Eastern')
            
@@ -71,7 +90,7 @@ def add_member(group_id, member_id):
     else:
         flash(member.username + " is already in " + group.group_name, category="error")
 
-    return redirect("/group-chat/" + group.group_name)
+    return redirect('/group-chat/' + str(group.group_id))
 
 @group_views.route('/leave-group/<string:group_chat>/<string:id>', methods=['GET', 'POST'])
 @login_required
@@ -80,8 +99,8 @@ def leave_group(group_chat, id):
     db.session.commit()
     return redirect('/')
 
-@group_views.route('/delete-message-group/<string:message_id>/<string:group_name>', methods=['GET', 'POST'])
-def delete_message_group(message_id, group_name):
+@group_views.route('/delete-message-group/<string:message_id>/<string:group_id>', methods=['GET', 'POST'])
+def delete_message_group(message_id, group_id):
     message = Message.query.get(message_id)
     
     if message:
@@ -89,4 +108,4 @@ def delete_message_group(message_id, group_name):
             db.session.delete(message)
             db.session.commit()
 
-    return redirect('/group-chat/' + group_name)
+    return redirect('/group-chat/' + str(group_id))
