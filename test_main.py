882a852fe492037819b82676eb5b53ab85ca7352
mcl7-upstate <77367175+mcl7-upstate@users.noreply.github.com>
2021-12-05 20:55:43 -0500

    unneeded files

--- test_main.py
+++
@@ -1,7 +0,0 @@
-# from flask import Flask, url_for
-
-# app = Flask(__name__)
-
-# with app.test_client() as c:
-#     response = c.get('/some/path/that/exists')
-#     self.assertEquals(response.status_code, 200)
